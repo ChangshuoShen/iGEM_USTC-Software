e614832a1cfc7b056601f312a124aa5776fc3c74
shenc <stephenshenc@gmail.com>
2024-10-10 21:05:46 +0800

    oct10 03

--- USTC-Software/USTC-Software/settings.py
+++ USTC-Software/USTC-Software/settings.py
@@ -152,13 +152,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
 USE_TZ = True
+
+LANGUAGE_CODE = "en-us" # 默认语言
+
+TIME_ZONE = "Asia/Shanghai"
+
 
 
 # Static files (CSS, JavaScript, Images)
